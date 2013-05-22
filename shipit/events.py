b66bca6676e35ae365f60eacc702c39cbcfa83c3
Alejandro Gómez <alejandro@dialelo.com>
2013-05-23 00:34:12 +0200

    Modernize the Python

--- shipit/events.py
+++ shipit/events.py
@@ -8,33 +8,33 @@ Poor man's pub-sub mechanism.
 """
 
 EVENTS = [
- "show_all",
- "show_created_by_you",
- "show_assigned_to_you",
- "show_mentioning_you",
+    "show_all",
+    "show_created_by_you",
+    "show_assigned_to_you",
+    "show_mentioning_you",
 
- "show_open_issues",
- "show_closed_issues",
- "show_pull_requests",
+    "show_open_issues",
+    "show_closed_issues",
+    "show_pull_requests",
 
- "filter_by_labels",
- "clear_label_filters",
+    "filter_by_labels",
+    "clear_label_filters",
 ]
 
 
-SUBSCRIBED = dict([(event,[]) for event in EVENTS])
+SUBSCRIBED = {event: [] for event in EVENTS}
+
 
 def trigger(event, *args, **kwargs):
     if event in EVENTS:
         for callback in SUBSCRIBED[event]:
             callback(*args, **kwargs)
     else:
-        raise ValueError("%s is not a valid event." % event)
+        raise ValueError("{} is not a valid event.".format(event))
+
 
 def on(event, callback):
     if event in EVENTS:
         SUBSCRIBED[event].append(callback)
     else:
-        raise ValueError("%s is not a valid event." % event)
-
-
+        raise ValueError("{} is not a valid event.".format(event))
